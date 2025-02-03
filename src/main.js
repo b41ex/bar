dc11b95e50739a321dc24570ddaa488ac41302ea
b41ex <bochencev.qubership@gmail.com>
2025-02-03 16:17:47 +0300

    feat: import from qux

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import foo1 from '@b41ex/foo';
+import qux from '@b41ex/qux';
 
-export default function () {	
+export default function _foo() {	
+	return foo1();
+}
+
+export default function _qux() {	
 	return foo1();
 }
