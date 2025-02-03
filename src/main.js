5698ed65bac1432262e352fbb061b805a3d8cb71
b41ex <bochencev.qubership@gmail.com>
2025-02-03 18:13:00 +0300

    fix: build

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import foo1 from '@b41ex/foo';
 import qux from '@b41ex/qux';
 
-export default function _foo() {	
+export function _foo() {	
 	return foo1();
 }
 
-export default function _qux() {	
+export function _qux() {	
 	return foo1();
 }
